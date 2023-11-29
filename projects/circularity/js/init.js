3dfce5c73c55838253577542204d11de52d5a6b5
KLewis82 <kjl4099998@lpssonline.com>
2023-11-29 16:14:34 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,12 +33,9 @@ var init = function (window) {
          }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-
+        for(var circlesDrawnLoop = 0; circlesDrawnLoop < 100; circlesDrawnLoop++){
+            drawCircle();
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -50,21 +47,16 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            for (var i = 0; i < circles.length; i++){
+                physikz.updatePosition(circles[i]);
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
+            for (var i = 0; i < circles.length; i++){
+                game.checkCirclePosition(circles[i]);
+            }
 
             // TODO 9 : Iterate over the array
-           
             
         }
     
@@ -81,10 +73,19 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if (circle.x < canvas.width) //What side is x not comming back from and continueing to leave?
-            if 
-            if 
-
+            // if circle has gone past the LEFT side of the screen then place it on the RIGHT.
+            //What sides is x and y not comming back from and continuing to leave?
+            if (circle.x < 0){
+                circle.x = canvas.width;
+            }
+            //if circle has gone past the BOTTOM of the screen then place it to the TOP.
+            if (circle.y > canvas.height){
+                circle.y = 0;
+            }
+            // if the circle has gone past the TOP of the screen then place it to the BOTTOM.
+            if (circle.y < 0){
+                circle.y = canvas.height;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
