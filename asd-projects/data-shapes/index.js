dbc9a8949931c3413dc1746a97bc517f8679a90c
KLewis82 <kjl4099998@lpssonline.com>
2024-02-21 16:20:52 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
     show: true,
     displayType: 0,
   };
+
   // This line produces most of the data array and stores it in the variable "dataShapes"
   const dataShapes = generateShapeData();
   var currentIndex = 0;
@@ -35,37 +36,39 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
-// "TODO 0 complete"
+//TODO 0 Complete
   // TODO 1: create a new shape object and add it to the array
   var shape = {
-    color : "blue",
-    shape : "circle",
-    repeat : 3
+    color: "blue",
+    shape: "circle",
+    repeat: 3,
   }
   dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
-  for ( var i = 0 ; i < dataShapes.length ; i++){
+  for (var i = 0 ; i < dataShapes.length ; i++){
     var currentShape = dataShapes[i];
-    if (currentShape["color"] === "red"){
+    if (currentShape[color] = "red"){
       currentShape.goodBehavior = "bounce";
-    }
-    else if (currentShape["color"] === "blue"){
+    } else if (currentShap[color] = "blue"){
       currentShape.goodBehavior = "blink";
     }
     else{
       currentShape.goodBehavior = "spin";
     }
   }
+
   // TODO 3-a: add a function that handles the static display type
   function handleStatic (data){
     setBackgroundWithObject (data);
     animationDetails.displayType = 1;
   }
+
   // TODO 4-a: add a function that handles the good display type
   function handleGood (color, shape, repeat){
     setBackgroundWithSimple (color, shape, repeat);
     animationDetails.displayType = 2;
   }
+
   // TODO 5-a: add a function that handles the bad display type
   function handleBad (data, repeat){
     repeat = repeat + 1;
@@ -77,9 +80,9 @@ $(document).ready(function () {
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
   /////////////////////////////////////////////////
 
-  function staticDisplay() {
+  function staticDisplay(){
     // TODO 3-b: call your handleStatic function
-    handleStatic (dataShapes[currentIndex]);
+    handleStatic(dataShapes[currentIndex]); 
   }
 
   function goodDisplay() {
@@ -90,10 +93,11 @@ $(document).ready(function () {
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    var currentShape =  dataShapes[currentIndex];
-    var repeat = currentShape["repeat"];
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape[repeat];
     handleBad (currentShape, repeat);
   }
+
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO ABOVE HERE ////////
   /////////////////////////////////////////////////
